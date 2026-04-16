62da6c8c1738aed6a839091298c1a7a98de3dff8
yohannes <yohannestagel21@gmail.com>
2026-04-16 23:31:22 +0300

    i add one  controller to worke logic part

---
+++ lib/controller/Home_controller.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+
+  String test = "test metode";
+
+  @override
+  void onInit() {
+
+    super.onInit();
+  }
+  testMethod(){
+  print(test);
+
+  }
+
+}
